0c54d033021d126badee3b09f6d7fb9fbd8363ee
Jahanara Khatun <zarakhatun58@gmail.com>
2025-12-02 01:37:18 +0530

    finish

--- .next/server/pages/_app.js
+++
@@ -1,1 +0,0 @@
-(()=>{var r={};r.id=888,r.ids=[888],r.modules={5913:(r,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>MyApp});var t=s(997);function MyApp({Component:r,pageProps:e}){return t.jsx(r,{...e})}s(6764)},6764:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var s=e(e.s=5913);module.exports=s})();
